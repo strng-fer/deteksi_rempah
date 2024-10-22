c98c98c24a4611b7601cb80145559e9742eb0ee5
Feryadi Yulius <133149348+strng-fer@users.noreply.github.com>
2024-10-22 20:12:41 +0700

    Update Deteksi_Rempah.py

--- pages/Deteksi_Rempah.py
+++ pages/Deteksi_Rempah.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+from PIL import Image
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
